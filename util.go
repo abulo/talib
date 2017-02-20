196a093f75618a61535a9cf9c1764f8d0a4c9ca4
x6doooo <x6doooo@gmail.com>
2017-02-20 23:45:11 +0800

    update

--- util.go
+++ util.go
@@ -2,13 +2,13 @@ package talib
 
 
 
-func formatFloat64(srcData []float64, begIdx, NBElement int) (outData []float64) {
+func FormatFloat64(srcData []float64, begIdx, NBElement int) (outData []float64) {
     outData = make([]float64, begIdx)
     outData = append(outData, srcData[0:NBElement]...)
     return
 }
 
-func formatInt(srcData []int, begIdx, NBElement int) (outData []int) {
+func FormatInt(srcData []int, begIdx, NBElement int) (outData []int) {
     outData = make([]int, begIdx)
     outData = append(outData, srcData[0:NBElement]...)
     return
